09a0b9d12897c0dffc40fd78f64098140e244a62
gabrielw6 <mercado.gw@gmail.com>
2017-11-10 11:41:01 -0200

    Allow a relay to have multiple days to turn on/off
    All relays may now be turned on/off simultaneously and on any day of the week

--- relay_main.c
+++ relay_main.c
@@ -1,51 +1,43 @@
-// Docs em http://www.rinkydinkelectronics.com/library.php?id=73
+// Docs em http://www.rinkydinkelectronics.com/library.php?id=73  
+// The following lines can be uncommented to set the date and time
+// (Add to setup() when needed)
+//rtc.setDOW(FRIDAY);     // Set Day-of-Week to SUNDAY
+//rtc.setTime(5, 6, 40);     // Set the time to 12:00:00 (24hr format)
+//rtc.setDate(8, 11, 2017);
 #include <DS3231.h>
 #include <string.h>
+#define N_R 4
 
-struct dow_schedule {
+typedef struct {
   int pin; // Pino do rele
-  char day[5]; // String com o dia
-  bool state; // Estado em que ele vai estar no dia
-}; // Cronograma de dia da semana
+  bool states[7]; // Estado em que ele vai estar no dia
+} dow_schedule; // Cronograma de dia da semana
 
-DS3231 rtc(SDA, SCL);
+int day_to_num(char *d) {
+  char days[7][5] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  for(int i=0; i<7; i++) {
+    if(!strcmp(days[i], d)) return i;
+  }
+  return -1;
+}
 
-dow_schedule relays[4];
+DS3231 rtc(SDA, SCL); //MEGA: SDA, SCL / Uno: A4, A5
+
 int c;
+
+dow_schedule relays[N_R] = {{.pin = 4, .states = {0,0,1,0,1,0,0}},
+                            {.pin = 5, .states = {0,1,0,1,0,0,1}},
+                            {.pin = 6, .states = {1,0,1,0,0,1,0}},
+                            {.pin = 7, .states = {0,1,0,0,1,0,1}}};
 
 void setup()
 { // Ligar um rele por dia. Pode-se escolher ligar mais de um
-  // tambem, eh so deixar os dias iuais e o state como true
-  relays[0].pin = 7;
-  strcpy(relays[0].day, "Fri");
-  relays[0].state = true;
+  // tambem, eh so deixar os dias iguais e o state como true
   
-  relays[1].pin = 6;
-  strcpy(relays[1].day, "Mon");
-  relays[1].state = true;
-  
-  relays[2].pin = 5;
-  strcpy(relays[2].day, "Wed");
-  relays[2].state = true;
-  
-  relays[3].pin = 4;
-  strcpy(relays[3].day, "Mon");
-  relays[3].state = true;
-  
-  for(int i=0; i<4; i++) pinMode(relays[i].pin, OUTPUT);
+  for(int i=0; i<N_R; i++) pinMode(relays[i].pin, OUTPUT);
   
   Serial.begin(9600);
-  // Comentarios abaixo para configurar o RTC (so usar uma vez)
-  // Uncomment the next line if you are using an Arduino Leonardo
-  //while (!Serial) {}
-  
-  // Initialize the rtc object
-  rtc.begin();
-  
-  // The following lines can be uncommented to set the date and time
-  //rtc.setDOW(FRIDAY);     // Set Day-of-Week to SUNDAY
-  //rtc.setTime(5, 6, 40);     // Set the time to 12:00:00 (24hr format)
-  //rtc.setDate(8, 11, 2017);
+  rtc.begin(); // Initalize RTC object
 
   c = 0; // Variavel contadora para loop
 }
@@ -53,12 +45,8 @@ void loop()
 {
   // Send Day-of-Week
   Serial.println(rtc.getDOWStr(FORMAT_SHORT));
-  if(!strcmp(rtc.getDOWStr(FORMAT_SHORT), relays[c].day)) {
-    if(relays[c].state) digitalWrite(relays[c].pin, HIGH);
-    else digitalWrite(relays[c].pin, LOW);
-  }
+  digitalWrite(relays[c].pin, relays[c].states[day_to_num(rtc.getDOWStr(FORMAT_SHORT))]);
   
-  // Wait one second before repeating
   delay (1000); // Gerenciar esse delay para um valor razoavel
   c += c==3 ? -3 : 1; // Resetar (ou incrementar) a variavel
 }
